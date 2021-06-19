8d8d1cde37e196b6a994240f2edfbacc3afdacb7
Adam Langbert <adamhl@pm.me>
2021-06-19 13:45:59 +0200

    set up project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import facebookLogin from 'ts-messenger-api'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const api = await facebookLogin({
+	email: process.env.EMAIL,
+	password: process.env.PASSWORD
+})
+
+if (!api) throw new Error('API failed to initialize.')
+
+const friends = await api.getFriendsList()
+
+console.log(friends)
